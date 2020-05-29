1c7323d4da4dc8792ba25eab7c1ad2c193e3b5f1
Vanille-N <neven.villani@gmail.com>
2020-05-29 09:38:20 +0200

    doc for canvas

--- src/canvas.rs
+++ src/canvas.rs
@@ -1,12 +1,15 @@
 use std::fs::File;
 use std::io::{BufWriter, Write};
 
+/// 8-bit RGB
 pub type Color = (u8, u8, u8);
 
+/// Indicates that a object of a given type can be converted to a color.
 pub trait Colorize<T = Self>: Copy {
     fn color(&self) -> Color;
 }
 
+/// A canvas is a 2D array of objects that we know how to convert to colors
 pub struct Canvas<T: Colorize> {
     hgt: usize,
     wth: usize,
@@ -14,6 +17,8 @@ pub struct Canvas<T: Colorize> {
 }
 
 impl<T: Colorize> Canvas<T> {
+    /// Fill with a default value
+    /// This is why the Copy trait is required
     pub fn new(hgt: usize, wth: usize, init: T) -> Self {
         Self {
             hgt,
@@ -22,6 +27,7 @@ impl<T: Colorize> Canvas<T> {
         }
     }
 
+    /// Output the current state of the canvas to a file
     pub fn render(&self, name: &str) {
         let mut f = BufWriter::new(File::create(name).unwrap());
         write!(f, "P3\n{} {}\n25\n", self.wth, self.hgt).unwrap();
@@ -34,6 +40,7 @@ impl<T: Colorize> Canvas<T> {
         f.flush().unwrap();
     }
 
+    /// Access array cells with wrapping
     pub fn mod_idx(&mut self, i: isize, j: isize) -> &mut T {
         &mut self.tab[mod_idx(i, self.hgt)][mod_idx(j, self.wth)]
     }
@@ -53,6 +60,7 @@ impl<T: Colorize> std::ops::IndexMut<[usize; 2]> for Canvas<T> {
     }
 }
 
+/// Wrap i to the range [0, n)
 fn mod_idx(i: isize, n: usize) -> usize {
     if i >= 0 {
         (i % n as isize) as usize
