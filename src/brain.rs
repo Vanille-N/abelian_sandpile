e8ac7359456e6253695eee306cd1f8702e58ce20
Vanille-N <neven.villani@gmail.com>
2020-05-29 09:31:20 +0200

    added doc for brain

--- src/brain.rs
+++ src/brain.rs
@@ -2,6 +2,7 @@ use rand::Rng;
 
 use crate::canvas::*;
 
+/// A cell in Brian's brain can have any of three states
 #[derive(Clone, Copy, Eq, PartialEq)]
 enum State {
     Ready,
@@ -9,9 +10,22 @@ enum State {
     Dying,
 }
 
+impl State {
+    /// Ready -> Firing -> Dying -> loop
+    pub fn next(self) -> State {
+        match self {
+            State::Ready => State::Firing,
+            State::Firing => State::Dying,
+            State::Dying => State::Ready,
+        }
+    }
+}
+
+/// A single cell in the Brian's Brain automaton
 #[derive(Clone, Copy)]
 struct Neuron {
     curr: State,
+    /// Changing the state has to be done after all cells have been checked
     succ: State,
 }
 
@@ -32,7 +46,9 @@ pub struct Brain {
     fired: usize,
 }
 
+/// A collection of Neurons
 impl Brain {
+    /// A new Brain is constructed from only its dimensions
     pub fn new(hgt: usize, wth: usize) -> Self {
         Self {
             field: Canvas::new(hgt, wth, Neuron::new()),
@@ -42,6 +58,8 @@ impl Brain {
         }
     }
 
+    /// Randomly fire some cells (all over the canvas)
+    /// p is the probability for any cell of being fired
     pub fn init_rand(&mut self, p: f64) {
         let mut rng = rand::thread_rng();
         for i in 0..self.hgt {
@@ -55,6 +73,10 @@ impl Brain {
         self.update();
     }
 
+    /// Randomly fire some cells (only in a small area near the center)
+    /// f is the fraction (both vertically and horizontally) of the canvas
+    /// on which to fire neurons.
+    /// p is the probability of any neuron of being fired
     pub fn init_cluster(&mut self, f: f64, p: f64) {
         let mut rng = rand::thread_rng();
         let lo = |n| (n as f64 * (1. - f) / 2.).floor() as usize;
@@ -70,6 +92,7 @@ impl Brain {
         self.update();
     }
 
+    /// Actualize current state with previously calculated next state
     pub fn update(&mut self) {
         for i in 0..self.hgt {
             for j in 0..self.wth {
@@ -78,6 +101,7 @@ impl Brain {
         }
     }
 
+    /// 2D Array access with automatic looping around the edges
     fn index_move(&self, i: usize, j: usize, mvi: isize, mvj: isize) -> [usize; 2] {
         let (mut i, mut j) = (i, j);
         match mvi {
@@ -119,6 +143,7 @@ impl Brain {
         [i, j]
     }
 
+    // Count neighbors (with looping around the edges) who are fired
     fn count_neigh(&self, i: usize, j: usize) -> usize {
         let mut res = 0;
         if self.field[self.index_move(i, j, -1, 0)].is_firing() {
@@ -148,6 +173,10 @@ impl Brain {
         res
     }
 
+    /// Rules of Brian's Brain for how a neuron changes state
+    /// Firing -> Dying
+    /// Dying -> Ready
+    /// Ready -> Firing iff 2 firing neighbors
     pub fn next(&mut self) {
         self.fired = 0;
         for i in 0..self.hgt {
@@ -171,6 +200,7 @@ impl Brain {
         self.update();
     }
 
+    /// Output current state of the canvas as an image
     pub fn render(&mut self, cfg: &mut crate::Config) {
         let name = cfg.frame();
         self.field.render(&name);
@@ -180,6 +210,7 @@ impl Brain {
 }
 
 impl Neuron {
+    /// All neurons are initialized ready to fire
     pub fn new() -> Self {
         Self {
             curr: State::Ready,
@@ -187,18 +218,12 @@ impl Neuron {
         }
     }
 
-    pub fn fire(&mut self) {
-        self.succ = State::Firing;
+    /// Ready -> Firing -> Dying -> loop
+    pub fn next(&mut self) {
+        self.succ = self.curr.next();
     }
 
-    pub fn kill(&mut self) {
-        self.succ = State::Ready;
-    }
-
-    pub fn refactor(&mut self) {
-        self.succ = State::Dying;
-    }
-
+    /// Set current state to calculated next state
     pub fn update(&mut self) {
         self.curr = self.succ;
     }
